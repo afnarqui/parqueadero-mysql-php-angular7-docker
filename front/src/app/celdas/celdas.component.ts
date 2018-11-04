895607d370c6d275aece205ecd5be4699db944c1
Andrés Felipe Naranjo Quintero <andresnaranjo@afnarqui.com>
2018-11-04 17:46:48 -0500

    CU10 refactorizar código

--- front/src/app/celdas/celdas.component.ts
+++ front/src/app/celdas/celdas.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CeldasService } from '../services/celdas/celdas.service';
-import { VehiculosParqueadosService } from '../services/vehiculosparqueados/vehiculos-parqueados.service';
 import { VehiculosParqueados } from '../interfaces/VehiculosParqueados';
+
 @Component({
   selector: 'app-celdas',
   templateUrl: './celdas.component.html',
@@ -17,72 +17,14 @@ export class CeldasComponent implements OnInit {
     created_at:null,
     update_at:null
 }
-  constructor(private servicioCeldas:CeldasService,private servicioVehiculosParqueadosService:VehiculosParqueadosService) { }
+  constructor(private servicioCeldas:CeldasService) { }
 
   ngOnInit() {
     this.traerDatos()
   }
   traerDatos(){
-    this.buscarCeldasOcupadas();
-  }
-
-  buscarCeldasOcupadas(){
-    this.servicioCeldas.get()
-    .subscribe((data)=>{
-      let dataAntes = JSON.stringify(data)
-      let dataValores = JSON.parse(dataAntes)
-
-      this.servicioVehiculosParqueadosService.get()
-        .subscribe((valoresparqueadero)=>{
-          if(valoresparqueadero){
-            let cadenavaloresparqueadero = JSON.stringify(valoresparqueadero)
-            let arrayvaloresparqueadero = JSON.parse(cadenavaloresparqueadero)
-              this.arrayvehiculosparqueadero =  arrayvaloresparqueadero
-              
-              for(let i = 0 ; i < dataValores.length;i++){
-                for(let k = 0;k < arrayvaloresparqueadero.length;k++){
-                  
-                  if(dataValores[i].id === arrayvaloresparqueadero[k].celdas_id && arrayvaloresparqueadero[k].estado == 'P'){
-                    
-                    dataValores[i]['color'] = 'card text-white bg-success mb-3'
-                    let placanueva = this.arrayvehiculos.filter((item)=>{
-                     
-                      
-                      return item.id ==arrayvaloresparqueadero[k].vehiculos_id
-                    })
-                    dataValores[i]['placa'] = placanueva[0].placa;
-                    
-                  }else{
-                  
-                  }
-                }
-              }
-              
-        
-             let nuevaData = this.agrupar(dataValores, 10 );
-
-             this.celdas = nuevaData
-             
-             
-          }else{
-            this.celdas = dataValores
-          }
-        },(error)=>{
-          console.log(error);
-        })
-    },
-    (error)=>{
-       console.log(error)
+    this.servicioCeldas.buscarCeldasOcupadas().then((data)=>{
+      this.celdas = data
     })
   }
-  private agrupar( arr:any, tamano:number ){
-    let nuevoArreglo = [];
-    for( let i = 0; i<arr.length; i+=tamano ){
-      nuevoArreglo.push( arr.slice(i, i+tamano) );
-    }
-
-    return nuevoArreglo;
-
-  }
-
 }
--- front/src/app/services/celdas/celdas.service.ts
+++ front/src/app/services/celdas/celdas.service.ts
@@ -1,15 +1,87 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { environment } from '../../../environments/environment'
-
+import { VehiculosParqueados } from 'src/app/interfaces/VehiculosParqueados';
+import { VehiculosParqueadosService } from '../vehiculosparqueados/vehiculos-parqueados.service';
 @Injectable({
   providedIn: 'root'
 })
 export class CeldasService {
   url = environment.URL_SERVICIOS_LOCALES;
-  constructor(private http:HttpClient) { }
+  celdas:any;
+  arrayvehiculosparqueadero:VehiculosParqueados[] = [];
+  arrayvehiculos:any = {
+    id:null,
+    placa:null,
+    marca:null,
+    created_at:null,
+    update_at:null
+}
+  constructor(private http:HttpClient,private servicioVehiculosParqueadosService:VehiculosParqueadosService) { }
   get(){
     return this.http.get(`${this.url}/celdas`)
+  }
+  buscarCeldasOcupadas(){
+    let promise = new Promise((resolve,reject)=>{
+      this.get()
+    .subscribe((data)=>{
+      let dataAntes = JSON.stringify(data)
+      let dataValores = JSON.parse(dataAntes)
+
+      this.servicioVehiculosParqueadosService.get()
+        .subscribe((valoresparqueadero)=>{
+          if(valoresparqueadero){
+            let cadenavaloresparqueadero = JSON.stringify(valoresparqueadero)
+            let arrayvaloresparqueadero = JSON.parse(cadenavaloresparqueadero)
+              this.arrayvehiculosparqueadero =  arrayvaloresparqueadero
+              
+              for(let i = 0 ; i < dataValores.length;i++){
+                for(let k = 0;k < arrayvaloresparqueadero.length;k++){
+                  
+                  if(dataValores[i].id === arrayvaloresparqueadero[k].celdas_id && arrayvaloresparqueadero[k].estado == 'P'){
+                    
+                    dataValores[i]['color'] = 'card text-white bg-success mb-3'
+                    let placanueva = this.arrayvehiculos.filter((item)=>{
+                     
+                      
+                      return item.id ==arrayvaloresparqueadero[k].vehiculos_id
+                    })
+                    dataValores[i]['placa'] = placanueva[0].placa;
+                    
+                  }else{
+                  
+                  }
+                }
+              }
+              
+        
+             let nuevaData = this.agrupar(dataValores, 10 );
+
+             this.celdas = nuevaData
+             resolve(this.celdas)
+             
+          }else{
+            this.celdas = dataValores
+            resolve(this.celdas)
+          }
+        },(error)=>{
+          reject([])
+        })
+    },
+    (error)=>{
+      reject([])
+    })
+    })
+    return promise
+  }
+  private agrupar( arr:any, tamano:number ){
+    let nuevoArreglo = [];
+    for( let i = 0; i<arr.length; i+=tamano ){
+      nuevoArreglo.push( arr.slice(i, i+tamano) );
+    }
+
+    return nuevoArreglo;
+
   }
 
 }
